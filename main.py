e6bc470149b3a7c45af89c39f1227eafd57a4d33
MaksTkachenko <maksym_pro@ukr.net>
2023-07-18 12:33:18 +0300

    second commit

--- main.py
+++ main.py
@@ -12,5 +12,14 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    count = 0
+    while True:
+        count += 1
+        print("h")
+
+        if count == 10:
+            break
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
